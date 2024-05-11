92e910aa13a170b5b057d91f5f0accf19b7394a7
Fernando de Oliveira Pereira <fernando.pereira@cemaden.gov.br>
2024-05-11 14:40:50 -0300

    refactory

---
+++ devframework/.distribution/apache-tomee/webapps/virtuallab/WEB-INF/classes/statics/js/app/Classes.js
@@ -0,0 +1,288 @@
+app.Classes = {
+		validExts: new Array(".class",".jar"),
+		tableClass: $('#classTable').DataTable({"language": app.settings.languagePtBr,
+			// desabilita a ordenacao na coluna do icone de remover servico:
+			"columnDefs": [{"orderable": false, "targets": 3}]}), 
+		
+		// funcao de inicializacao 
+		init: function () {
+			
+			app.settings.loading.show();
+			
+			// apaga o storage atual
+			app.storage.clear();
+
+			// lista as classes
+			app.Classes.list();
+	    	
+	    	var validExts = new Array(".class",".jar");
+	    	$("#uploadFile").attr("accept", app.Classes.validExts);
+	    	
+	    	$("#uploadFile").on("change", function (event) {
+	    		$("#inputGroupFile01").text($(this).val());
+	    	});
+	    	
+	    	$("#btnCancel").on("click", function (event) {
+	    		$('#uploadFile').replaceWith($('#uploadFile').val('').clone(true));
+	    		$("#inputGroupFile01").text("Selecione uma classe java");
+	            $('#classModal').modal('hide');
+	    	});
+	    	
+	    	$("#btnSubmit").click(function (event) {
+				event.preventDefault();
+				app.Classes.save();
+	        });
+	    	
+	    	// lista os metodos da classe de servico selecionada
+	    	$(document).on('click', '.listMethods', function(){
+	    		event.preventDefault();
+	    		$index = $(this).data("index");
+	    		app.Classes.listMethods($index);
+	    	});
+	    	
+	    	// remove a classe de servico selecionada
+	    	$(document).on('click', '.removeService', function(){
+	    		event.preventDefault();
+	    		$index = $(this).data("index");
+	    		app.Classes.removeService($index);
+	    	});
+		},
+		
+		// carrega a lista de classes
+		list: function(){
+			
+        	app.Classes.tableClass.rows().remove().draw();
+        	
+        	$.ajax({
+                type: "GET",
+                url: $('#classTable').data("url"),
+                processData: false,
+                contentType: false,
+                cache: false,
+                timeout: 600000,
+                success: function (result) {
+                    console.log(result);
+                    if (result.success){
+                    	var classList = result.data;
+                        
+                        // armazena as classes recebidas no storage
+                        app.storage.put("classList", classList);
+                    	
+	                    $.each(classList, function( i, classDesc ) {	                    	
+	                    	app.Classes.tableClass.row.add([
+	                    		classDesc.label,
+								"<a href='#' class='listMethods' data-index='" + i + "'>" + classDesc.qualifiedName + "</a>",
+								classDesc.description,
+								"<center><a href='#' class='removeService' data-index='" + i + "'><img src='resources/images/delete.png' height='25' width='25'></a></center>",
+								]).draw( false );
+	                   	});
+                    }else{
+                    	alertBt({
+       	        	      messageText: result.message,
+       	        	      headerText: "Alert",
+       	        	      alertType: "danger"
+       	        	    });
+                    }
+        			app.settings.loading.hide();
+                },
+                error: function (e) {
+                	var $msg = $(e.responseText).filter('title').text();
+                	if ($msg == '') $msg = "An error has occurred.<br/>";
+                    alertBt({
+   	        	      messageText: $msg,
+   	        	      headerText: "Error",
+   	        	      alertType: "danger"
+   	        	    });
+        			app.settings.loading.hide();
+                }
+            });
+		},
+		
+		// carrega os metodos da classe selecionada
+		listMethods: function(index){
+			
+			// recupera o descritor da classe escolhida
+    		var classDesc = app.storage.get("classList")[index];
+
+			// JSON de request
+    		var jsonReq = new Object();
+    		jsonReq.clazz = classDesc.qualifiedName;
+    		
+    		
+    		$.ajax({
+    			url: 'listMethods.op',
+    			type: 'POST',
+    			dataType: 'json',
+    			data: JSON.stringify(jsonReq),
+    			contentType: 'application/json',
+    			mimeType: 'application/json',
+    			success: function (result) {
+    				console.log(result);
+    				
+    				// apaga o storage atual
+    				app.storage.clear();
+    				
+    				// armazena o descritor da classe selecionada
+    				app.storage.put("classDescriptor", classDesc);
+    				
+    				// armazena o objeto recebido no storage
+    				app.storage.put("methodList", result.data);
+    				
+    				// redireciona para a pagina de metodos
+    				app.callPage("methods.jsp");
+    	        },
+    			error:function(data,status,er) {
+    				alert("error");
+    			}
+    		});
+		},
+		
+		// remove a classe de servico selecionada
+		removeService: function(index){
+			
+			// recupera o descritor da classe escolhida
+    		var classDesc = app.storage.get("classList")[index];
+
+			// JSON de request
+    		var jsonReq = new Object();
+    		jsonReq.clazz = classDesc.qualifiedName;
+    		
+    		// confirma apagar o servico
+    		var promise = alertBt({
+    		  type: "confirm",
+      	      messageText: "Do you really want to delete the module? '" + classDesc.label + "'?",
+      	      headerText: "Alert",
+      	      alertType: "warning"
+      	    });
+    		
+    		// processa a escolha do usuario
+    		promise.done(function (dlgResult) {
+    			
+    			// confirmou apagar o servico
+    			if ( dlgResult ){
+    				
+    	    		$.ajax({
+    	    			url: 'removeService.op',
+    	    			type: 'POST',
+    	    			dataType: 'json',
+    	    			data: JSON.stringify(jsonReq),
+    	    			contentType: 'application/json',
+    	    			mimeType: 'application/json',
+    	    			success: function (result) {
+    	    				console.log(result);
+    	    				
+    	                    if (result.success){
+    	                        alertBt({
+    	         	        	      messageText: result.message,
+    	         	        	      headerText: "Confirmation",
+    	         	        	      alertType: "success"
+    	         	        	    });
+    	                        
+    	                          // lista as classes de servico
+    	                          app.Classes.list();
+    	                      }else{
+    	                      	alertBt({
+    	           	        	      messageText: result.message,
+    	           	        	      headerText: "Alert",
+    	           	        	      alertType: "warning"
+    	           	        	    });
+    	                      }
+    	    	        },
+    	    			error:function(data,status,er) {
+    	                    var $msg = $(er.responseText).filter('title').text();
+    	                	if ($msg == '') $msg = "An error has occurred.<br/><br/><b>Erro</b>: " + er + ".";
+    	                    alertBt({
+    	   	        	      messageText: $msg,
+    	   	        	      headerText: "Error",
+    	   	        	      alertType: "danger"
+    	   	        	    });
+    	                }
+    	    		});
+    			}
+    		});
+		},
+		
+		// faz o upload do arquivo (classe/jar) selecionado 
+		save: function () {
+
+			var $form = $('#fileUploadForm');
+            
+            if ($('#uploadFile').val() == ""){
+            	alertBt({
+	        	      messageText: "Select a java class, file of type " + app.Classes.validExts.toString() + ".",
+	        	      headerText: "Alert",
+	        	      alertType: "warning"
+	        	    });
+            	return false;
+            }else{
+            	var $fileExt = $('#uploadFile').val();
+                $fileExt = $fileExt.substring($fileExt.lastIndexOf('.'));
+                if (app.Classes.validExts.indexOf($fileExt) < 0) {
+					alertBt({
+	        	      messageText: "The selected file is invalid. Select only files of type " + app.Classes.validExts.toString() + ".",
+	        	      headerText: "Alert",
+	        	      alertType: "warning"
+	        	    });
+					$("#inputGroupFile01").text("Selecione uma classe java");
+					$('#uploadFile').replaceWith($('#uploadFile').val('').clone(true));
+                  return false;
+                }
+            }
+            
+            $("#btnSubmit").attr("disabled", true);
+            
+            var $data = new FormData($form[0]);
+            $.ajax({
+                type: "POST",
+                enctype: 'multipart/form-data',
+                url: $form.attr('action'),
+                data: $data,
+                processData: false,
+                contentType: false,
+                cache: false,
+                timeout: 600000,
+                success: function (result) {
+                    console.log(result);
+                    $("#btnSubmit").attr("disabled", false);
+                    
+                    if (result.success){
+                    	$('#uploadFile').replaceWith($('#uploadFile').val('').clone(true));
+                        $("#inputGroupFile01").text("Select a java class");
+                        $('#classModal').modal('hide');
+                        alertBt({
+       	        	      messageText: result.message,
+       	        	      headerText: "Confirmation",
+       	        	      alertType: "success"
+       	        	    });
+                        
+                        // lista as classes de servico
+                        app.Classes.list();
+                    }else{
+                    	alertBt({
+         	        	      messageText: result.message,
+         	        	      headerText: "Alert",
+         	        	      alertType: "warning"
+         	        	    });
+                    }                    
+                },
+                error: function (e) {
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#uploadFile').replaceWith($('#uploadFile').val('').clone(true));
+                    $("#inputGroupFile01").text("Select a java class");
+                    $('#classModal').modal('hide');
+
+                    var $msg = $(e.responseText).filter('title').text();
+                	if ($msg == '') $msg = "An error has occurred.<br/><br/><b>Erro</b>: " + e + ".";
+                    alertBt({
+   	        	      messageText: $msg,
+   	        	      headerText: "Error",
+   	        	      alertType: "danger"
+   	        	    });
+                }
+            });
+		}
+};
+
+$(function() {
+    app.Classes.init();
+});
---
+++ devframework/.distribution/apache-tomee/webapps/virtuallab/invokeMethod.jsp
@@ -0,0 +1,79 @@
+<%@ taglib tagdir="/WEB-INF/tags" prefix="tags" %>
+
+<tags:_Layout>
+
+	<jsp:attribute name="css_custom">
+		<link rel="stylesheet" type="text/css" href="webjars/chartjs/2.8.0/Chart.min.css">
+	</jsp:attribute>
+	
+	<jsp:attribute name="content">
+		<main role="main">
+			
+			<nav aria-label="breadcrumb">
+				<ol class="breadcrumb">
+			    	<li class="breadcrumb-item">
+			    		<a href="./">Class</a>
+			    		<i class="fas fa-angle-right mx-2" aria-hidden="true"></i>
+			    		
+			    	</li>
+			    	<li class="breadcrumb-item active" aria-current="page">
+			    		<a href="methods.jsp" id="breadcrumbClassName"/></a>
+			    		<i class="fas fa-angle-right mx-2" aria-hidden="true"></i>
+			    	</li>
+			    	<li class="breadcrumb-item active" aria-current="page" id="breadcrumbMethodName"/>
+				</ol>
+			</nav>
+
+			<section class="jumbotron text-center">
+				<div class="container">
+					<h1 class="jumbotron-heading">Service: <strong id="headerMethodName"></strong> of the Module: <strong id="headerClassName"></strong> </h1>
+				</div>
+			</section>
+			<div class="clearfix my-1">&nbsp;</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="card">
+	  						<div class="card-header">
+								Enter the method parameters <strong>"<span id="InvokeMethodName"></span>"</strong> :
+							</div>
+							<div class="card-body">
+								<form id="formParam" data-url="invokeMethod.op"></form>
+	  						</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix my-1">&nbsp;</div>
+				<div class="row">
+					<div class="col-md-12">
+						<div class="card" id="tabResult">
+	  						<div class="card-header">
+								Result :
+							</div>
+							<div class="card-body">
+								<div id="result"></div>
+	  						</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix my-2">&nbsp;</div>
+			</div>
+			<div>
+				
+			</div>
+			<div>
+				
+			</div>
+	
+		</main>
+		
+	</jsp:attribute>
+	
+	<jsp:attribute name="js_custom">
+		<!-- load chartjs library -->
+		<script type="text/javascript" src="webjars/momentjs/2.24.0/moment.js"></script>
+		<script type="text/javascript" src="webjars/chartjs/2.8.0/Chart.min.js"></script>
+		<script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/app/RenderResult.js"></script>
+		<script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/app/InvokeMethod.js"></script>
+	</jsp:attribute>
+</tags:_Layout>
