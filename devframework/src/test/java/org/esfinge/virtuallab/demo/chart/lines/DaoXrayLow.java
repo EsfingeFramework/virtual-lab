ed478a37dd5e822aace4dabe6fc2ffcf5a136ea8
Fernando de Oliveira Pereira <fernando.pereira@cemaden.gov.br>
2024-05-11 22:05:50 -0300

    refactory

--- devframework/src/test/java/org/esfinge/virtuallab/demo/chart/lines/DaoXrayLow.java
+++ devframework/src/test/java/org/esfinge/virtuallab/demo/chart/lines/DaoXrayLow.java
@@ -1,50 +1,48 @@
 package org.esfinge.virtuallab.demo.chart.lines;
 
+import esfinge.querybuilder.core.Repository;
+import esfinge.querybuilder.core.annotation.GreaterOrEquals;
 import java.util.List;
 
 import org.esfinge.virtuallab.api.annotations.ServiceDAO;
 import org.esfinge.virtuallab.api.annotations.ServiceMethod;
 import org.esfinge.virtuallab.api.annotations.TableReturn;
 
-import net.sf.esfinge.querybuilder.Repository;
-import net.sf.esfinge.querybuilder.annotation.GreaterOrEquals;
 
 /*--------------------------------------------------------------------------
  * Demonstracao da anotacao @ServiceDAO.
  *-------------------------------------------------------------------------*/
 @ServiceDAO(
-	label = "DAO",
-	description = "Demonstração da anotação @ServiceDAO.",
-	url = "jdbc:postgresql://localhost:5432/climaespacial",
-	user = "postgres", 					    
-	password = "postgres", 
-	dialect = "org.hibernate.dialect.PostgreSQLDialect")
-public interface DaoXrayLow extends Repository<XrayDataLow>
-{
-	    @ServiceMethod(
-			label = "Listar os valores de goes.xray_data_low",
-			description = "Retorna todos os elementos da tabela goes.xray_data_low")
-		@TableReturn
-		public List<XrayDataLow> getXrayDataLow();
-	    
-	    @ServiceMethod(
-				label = "Listar os valores de goes.xray_data_low",
-				description = "Retorna todos os elementos da tabela goes.xray_data_low")
-		@TableReturn
-		public List<XrayDataLow> getXrayDataLowById(@GreaterOrEquals Long id);
-	    
-	    @ServiceMethod(
-				label = "Listar os valores de goes.xray_data_low filtrando por header",
-				description = "Retorna todos os elementos da tabela goes.xray_data_low")
-		@TableReturn
-		public List<XrayDataLow> getXrayDataLowByIdAndHeader(@GreaterOrEquals Long id,Long header);
-	    
-	    
-	    @ServiceMethod(
-				label = "Listar os valores de goes.xray_data_low filtrando por data",
-				description = "Retorna todos os elementos da tabela goes.xray_data_low")
-		@TableReturn
-	    public List<XrayDataLow> getXrayDataLowByHeaderOrderById(Long header);
+        label = "DAO",
+        description = "Demonstração da anotação @ServiceDAO.",
+        url = "jdbc:postgresql://localhost:5432/climaespacial",
+        user = "postgres",
+        password = "postgres",
+        dialect = "org.hibernate.dialect.PostgreSQLDialect")
+public interface DaoXrayLow extends Repository<XrayDataLow> {
 
+    @ServiceMethod(
+            label = "Listar os valores de goes.xray_data_low",
+            description = "Retorna todos os elementos da tabela goes.xray_data_low")
+    @TableReturn
+    public List<XrayDataLow> getXrayDataLow();
+
+    @ServiceMethod(
+            label = "Listar os valores de goes.xray_data_low",
+            description = "Retorna todos os elementos da tabela goes.xray_data_low")
+    @TableReturn
+    public List<XrayDataLow> getXrayDataLowById(@GreaterOrEquals Long id);
+
+    @ServiceMethod(
+            label = "Listar os valores de goes.xray_data_low filtrando por header",
+            description = "Retorna todos os elementos da tabela goes.xray_data_low")
+    @TableReturn
+    public List<XrayDataLow> getXrayDataLowByIdAndHeader(@GreaterOrEquals Long id, Long header);
+
+    @ServiceMethod(
+            label = "Listar os valores de goes.xray_data_low filtrando por data",
+            description = "Retorna todos os elementos da tabela goes.xray_data_low")
+    @TableReturn
+    public List<XrayDataLow> getXrayDataLowByHeaderOrderById(Long header);
 
 }
--- devframework/src/test/java/org/esfinge/virtuallab/domain/TopicService.java
+++ devframework/src/test/java/org/esfinge/virtuallab/domain/TopicService.java
@@ -1,20 +1,19 @@
 package org.esfinge.virtuallab.domain;
 
+import esfinge.querybuilder.core.Repository;
 import java.util.List;
 
 import org.esfinge.virtuallab.api.annotations.ServiceDAO;
 import org.esfinge.virtuallab.api.annotations.ServiceMethod;
 
-import net.sf.esfinge.querybuilder.Repository;
-
 @ServiceDAO(label = "DAO Topics",
-description = "Banco de Dados: MySQL  -  Entidade: Topic",
-url = "jdbc:mysql://localhost:3306/querybuilder", user = "root", password = "", dialect = "org.hibernate.dialect.MySQL8Dialect")
-public interface TopicService extends Repository<Topic>
-{
-	@ServiceMethod(label = "Obter todos os topicos", description = "Retorna todos os topicos cadastrados no Banco de Dados")
-	public List<Topic> getTopic();
-	
-	@ServiceMethod(label = "Obter topicos por nome", description = "Retorna os topicos cadastrados no Banco de Dados com o nome informado")
-	public List<Topic> getTopicByName(String name);
+        description = "Banco de Dados: MySQL  -  Entidade: Topic",
+        url = "jdbc:mysql://localhost:3306/querybuilder", user = "root", password = "", dialect = "org.hibernate.dialect.MySQL8Dialect")
+public interface TopicService extends Repository<Topic> {
+
+    @ServiceMethod(label = "Obter todos os topicos", description = "Retorna todos os topicos cadastrados no Banco de Dados")
+    public List<Topic> getTopic();
+
+    @ServiceMethod(label = "Obter topicos por nome", description = "Retorna os topicos cadastrados no Banco de Dados com o nome informado")
+    public List<Topic> getTopicByName(String name);
 }
